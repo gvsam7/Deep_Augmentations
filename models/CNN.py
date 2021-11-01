e7cc918827fe57fb39678021c4fd6919fe06e105
Georgios Voulgaris <gvsam7@yahoo.com>
2021-11-01 14:07:41 +0000

    CNN5 debugging

--- models/CNN.py
+++ models/CNN.py
@@ -407,39 +407,40 @@ class CNN5(nn.Module):
         x = self.fc1(x)
         return x"""
 
+
 class CNN5(nn.Module):
-  def __init__(self, in_channels=3, num_classes=9):
-    super(CNN5, self).__init__()
+    def __init__(self, in_channels, num_classes):
+        super(CNN5, self).__init__()
 
-    self.features = nn.Sequential(
-        nn.Conv2d(in_channels, 32, 3, 1, 1),
-        nn.ReLU(inplace=True),
-        nn.MaxPool2d((2, 2), (2, 2)),
-        nn.BatchNorm2d(32),
-        nn.Conv2d(32, 64, 3, 1, 1),
-        nn.ReLU(inplace=True),
-        nn.MaxPool2d((2, 2), (2, 2)),
-        nn.BatchNorm2d(64),
-        nn.Conv2d(64, 128, 3, 1, 1),
-        nn.ReLU(inplace=True),
-        nn.MaxPool2d((2, 2), (2, 2)),
-        nn.BatchNorm2d(128),
-        nn.Conv2d(128, 256, 3, 1, 1),
-        nn.ReLU(inplace=True),
-        nn.MaxPool2d((2, 2), (2, 2)),
-        nn.BatchNorm2d(256),
-        nn.Conv2d(256, 512, 3, 1, 1)
-    )
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels, 32, 3, 1, 1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d((2, 2), (2, 2)),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 64, 3, 1, 1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d((2, 2), (2, 2)),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 128, 3, 1, 1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d((2, 2), (2, 2)),
+            nn.BatchNorm2d(128),
+            nn.Conv2d(128, 256, 3, 1, 1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d((2, 2), (2, 2)),
+            nn.BatchNorm2d(256),
+            nn.Conv2d(256, 512, 3, 1, 1)
+        )
 
-    self.pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.pool = nn.AdaptiveAvgPool2d((1, 1))
 
-    self.classifier = nn.Sequential(
-        nn.Linear(512, num_classes)
-    )
+        self.classifier = nn.Sequential(
+            nn.Linear(512, num_classes)
+        )
 
-  def forward(self, x):
-    x = self.features(x)
-    x = self.pool(x)
-    x = x.flatten(1)
-    x = self.classifier(x)
-    return x
+    def forward(self, x):
+        x = self.features(x)
+        x = self.pool(x)
+        x = x.flatten(1)
+        x = self.classifier(x)
+        return x
