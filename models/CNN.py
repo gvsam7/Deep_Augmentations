8cb29e52141efdda5f8177d5826f1dcea0c8432b
Georgios Voulgaris <gvsam7@yahoo.com>
2021-11-01 11:29:16 +0000

    CNN5 sequential

--- models/CNN.py
+++ models/CNN.py
@@ -327,6 +327,8 @@ class FCNN4(nn.Module):
         self.bn1 = nn.BatchNorm2d(32)
 
 
+"""
+# prototype 96% Accuracy
 class CNN5(nn.Module):
     def __init__(self, in_channels=3, num_classes=10):
         super(CNN5, self).__init__()
@@ -403,4 +405,41 @@ class CNN5(nn.Module):
         # x = x.reshape(x.shape[0], -1)
         x = x.flatten(1)
         x = self.fc1(x)
-        return x
+        return x"""
+
+class CNN5(nn.Module):
+  def __init__(self, in_channels=3, num_classes=9):
+    super(CNN5, self).__init__()
+
+    self.features = nn.Sequential(
+        nn.Conv2d(in_channels, 32, 3, 1, 1),
+        nn.ReLU(inplace=True),
+        nn.MaxPool2d((2, 2), (2, 2)),
+        nn.BatchNorm2d(32),
+        nn.Conv2d(32, 64, 3, 1, 1),
+        nn.ReLU(inplace=True),
+        nn.MaxPool2d((2, 2), (2, 2)),
+        nn.BatchNorm2d(64),
+        nn.Conv2d(64, 128, 3, 1, 1),
+        nn.ReLU(inplace=True),
+        nn.MaxPool2d((2, 2), (2, 2)),
+        nn.BatchNorm2d(128),
+        nn.Conv2d(128, 256, 3, 1, 1),
+        nn.ReLU(inplace=True),
+        nn.MaxPool2d((2, 2), (2, 2)),
+        nn.BatchNorm2d(256),
+        nn.Conv2d(256, 512, 3, 1, 1)
+    )
+
+    self.pool = nn.AdaptiveAvgPool2d((1, 1))
+
+    self.classifier = nn.Sequential(
+        nn.Linear(512, num_classes)
+    )
+
+  def forward(self, x):
+    x = self.features(x)
+    x = self.pool(x)
+    x = x.flatten(1)
+    x = self.classifier(x)
+    return x
