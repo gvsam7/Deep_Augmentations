8e6779bacd59e933e4572dbca82e60efafdc712c
Georgios Voulgaris <gvsam7@yahoo.com>
2021-11-19 12:32:07 +0000

    mix pool

--- models/GaborCNN.py
+++ models/GaborCNN.py
@@ -9,39 +9,32 @@ class GaborCNN(nn.Module):
     def __init__(self, in_channels, num_classes):
         super(GaborCNN, self).__init__()
         self.features = nn.Sequential(
-            GaborConv2d(in_channels, out_channels=32, kernel_size=(15, 15)),
+            GaborConv2d(in_channels, out_channels=32, kernel_size=(11, 11)),
             nn.ReLU(inplace=True),
             # GatedPool_l(kernel_size=2, stride=2, padding=0),
-            GatedPool_c(in_channels=32, kernel_size=2, stride=2, padding=0),
-            # MixPool(2, 2, 0, 1),
-            # nn.MaxPool2d(2),
-            nn.BatchNorm2d(32),
-            nn.Conv2d(32, 32, (3, 3)),
-            nn.ReLU(inplace=True),
-            # GatedPool_l(kernel_size=2, stride=2, padding=0),
-            GatedPool_c(in_channels=32, kernel_size=2, stride=2, padding=0),
-            # MixPool(2, 2, 0, 0.8),
+            # GatedPool_c(in_channels=32, kernel_size=2, stride=2, padding=0),
+            MixPool(2, 2, 0, 1),
             # nn.MaxPool2d(2),
             nn.BatchNorm2d(32),
             nn.Conv2d(32, 64, (3, 3)),
             nn.ReLU(inplace=True),
             # GatedPool_l(kernel_size=2, stride=2, padding=0),
-            GatedPool_c(in_channels=64, kernel_size=2, stride=2, padding=0),
-            # MixPool(2, 2, 0, 0.6),
+            # GatedPool_c(in_channels=64, kernel_size=2, stride=2, padding=0),
+            MixPool(2, 2, 0, 0.6),
             # nn.MaxPool2d(2),
             nn.BatchNorm2d(64),
             nn.Conv2d(64, 128, (3, 3)),
             nn.ReLU(inplace=True),
             # GatedPool_l(kernel_size=2, stride=2, padding=0),
-            GatedPool_c(in_channels=128, kernel_size=2, stride=2, padding=0),
-            # MixPool(2, 2, 0, 0.2),
+            # GatedPool_c(in_channels=128, kernel_size=2, stride=2, padding=0),
+            MixPool(2, 2, 0, 0.2),
             # nn.MaxPool2d(2),
             nn.BatchNorm2d(128),
             nn.Conv2d(128, 256, (3, 3)),
             nn.ReLU(inplace=True),
             # GatedPool_l(kernel_size=2, stride=2, padding=0),
-            GatedPool_c(in_channels=256, kernel_size=2, stride=2, padding=0),
-            # MixPool(2, 2, 0, 0.2),
+            # GatedPool_c(in_channels=256, kernel_size=2, stride=2, padding=0),
+            MixPool(2, 2, 0, 0.2),
             # nn.MaxPool2d(2),
             nn.BatchNorm2d(256),
             nn.Conv2d(256, 512, (3, 3)),
@@ -51,9 +44,7 @@ class GaborCNN(nn.Module):
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
 
         self.classifier = nn.Sequential(
-            nn.Linear(512, 512),
-            nn.ReLU(inplace=True),
-            nn.Linear(512, num_classes)
+           nn.Linear(512, num_classes)
         )
 
     def forward(self, x):
