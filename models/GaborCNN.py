05a354b0fe261e30f3a4a07141a80ed7215896e6
Georgios Voulgaris <gvsam7@yahoo.com>
2021-11-24 12:30:22 +0000

    3x3 Gabor CNN

--- models/GaborCNN.py
+++ models/GaborCNN.py
@@ -10,36 +10,36 @@ class GaborCNN(nn.Module):
     def __init__(self, in_channels, num_classes, num_level=3, pool_type='average_pool'):
         super(GaborCNN, self).__init__()
         self.features = nn.Sequential(
-            GaborConv2d(in_channels, out_channels=32, kernel_size=(11, 11)),
-            nn.SELU(inplace=True),
+            GaborConv2d(in_channels, out_channels=32, kernel_size=(3, 3)),
+            nn.ReLU(inplace=True),
             # GatedPool_l(kernel_size=2, stride=2, padding=0),
             # GatedPool_c(in_channels=32, kernel_size=2, stride=2, padding=0),
             MixPool(2, 2, 0, 1),
             # nn.MaxPool2d(2),
             nn.BatchNorm2d(32),
             nn.Conv2d(32, 64, (3, 3)),
-            nn.SELU(inplace=True),
+            nn.ReLU(inplace=True),
             # GatedPool_l(kernel_size=2, stride=2, padding=0),
             # GatedPool_c(in_channels=64, kernel_size=2, stride=2, padding=0),
             MixPool(2, 2, 0, 0.6),
             # nn.MaxPool2d(2),
             nn.BatchNorm2d(64),
             nn.Conv2d(64, 128, (3, 3)),
-            nn.SELU(inplace=True),
+            nn.ReLU(inplace=True),
             # GatedPool_l(kernel_size=2, stride=2, padding=0),
             # GatedPool_c(in_channels=128, kernel_size=2, stride=2, padding=0),
             MixPool(2, 2, 0, 0.2),
             # nn.MaxPool2d(2),
             nn.BatchNorm2d(128),
             nn.Conv2d(128, 256, (3, 3)),
-            nn.SELU(inplace=True),
+            nn.ReLU(inplace=True),
             # GatedPool_l(kernel_size=2, stride=2, padding=0),
             # GatedPool_c(in_channels=256, kernel_size=2, stride=2, padding=0),
             MixPool(2, 2, 0, 0.2),
             # nn.MaxPool2d(2),
             nn.BatchNorm2d(256),
             nn.Conv2d(256, 512, (3, 3)),
-            nn.SELU(inplace=True)
+            nn.ReLU(inplace=True)
         )
 
         # self.avgpool = SPP(num_level)
