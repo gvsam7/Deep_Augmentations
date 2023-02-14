a2a19f0e9f609ed1659f68acd76857b31b57bee6
Georgios Voulgaris <gvsam7@yahoo.com>
2023-02-14 14:02:56 +0000

    Modified Mix Pool

--- models/Block.py
+++ models/Block.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from Pool.MixPool import MixPool
 
 
 class block(nn.Module):
@@ -46,6 +47,40 @@ class block_18(nn.Module):
         self.bn1 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU()
         self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+    def forward(self, x):
+        identity = x.clone()
+
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+
+        if self.identity_downsample is not None:
+            identity = self.identity_downsample(identity)
+
+        x += identity
+        x = self.relu(x)
+        return x
+
+
+class mpblock_18(nn.Module):
+    def __init__(self, in_channels, out_channels, identity_downsample=None, stride=1):
+        super(mpblock_18, self).__init__()
+        self.identity_downsample = identity_downsample
+        self.stride = stride
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU()
+        if stride != 1:
+            self.conv2 = nn.Sequential(
+                nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
+                MixPool(2, 2, 0, 0.8)
+            )
+        else:
+            self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(out_channels)
 
 
--- models/MPDilGabResNet18.py
+++ models/MPDilGabResNet18.py
@@ -1,12 +1,12 @@
 import torch.nn as nn
-from models.Block import block_18
+from models.Block import mpblock_18
 from Gabor.GaborLayer import GaborConv2d
 from models.ConvBlock import DACBlock
 from Pool.MixPool import MixPool
 
 
 class ResNet(nn.Module):
-    def __init__(self, block_18, layers, in_channels, num_classes):
+    def __init__(self, mpblock_18, layers, in_channels, num_classes):
         super(ResNet, self).__init__()
         self.in_channels = 64
         self.conv1 = GaborConv2d(in_channels, 64, kernel_size=7, stride=2, padding=3)
@@ -15,10 +15,10 @@ class ResNet(nn.Module):
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
         # ResNet layers
-        self.layer1 = self._make_layer(block_18, layers[0], out_channels=64, stride=1)
-        self.layer2 = self._make_layer(block_18, layers[1], out_channels=128, stride=2)
-        self.layer3 = self._make_layer(block_18, layers[2], out_channels=256, stride=2)
-        self.layer4 = self._make_layer(block_18, layers[3], out_channels=512, stride=2)  # 512
+        self.layer1 = self._make_layer(mpblock_18, layers[0], out_channels=64, stride=1)
+        self.layer2 = self._make_layer(mpblock_18, layers[1], out_channels=128, stride=2)
+        self.layer3 = self._make_layer(mpblock_18, layers[2], out_channels=256, stride=2)
+        self.layer4 = self._make_layer(mpblock_18, layers[3], out_channels=512, stride=2)  # 512
 
         self.dil = DACBlock(512, 512)
 
@@ -44,27 +44,25 @@ class ResNet(nn.Module):
 
         return x
 
-    def _make_layer(self, block_18, num_residual_blocks, out_channels, stride):
+    def _make_layer(self, mpblock_18, num_residual_blocks, out_channels, stride):
         identity_downsample = None
         layers = []
 
         if stride != 1 or self.in_channels != out_channels * 1:
             identity_downsample = nn.Sequential(
-                # nn.Conv2d(self.in_channels, out_channels, kernel_size=1, stride=stride),
                 nn.Conv2d(self.in_channels, out_channels, kernel_size=1, stride=1),
-                # nn.MaxPool2d(2),
                 MixPool(2, 2, 0, 0.8),
                 nn.BatchNorm2d(out_channels),
             )
 
-        layers.append(block_18(self.in_channels, out_channels, identity_downsample, stride))
+        layers.append(mpblock_18(self.in_channels, out_channels, identity_downsample, stride))
         self.in_channels = out_channels  # 64*1=64
 
         for i in range(num_residual_blocks - 1):
-            layers.append(block_18(self.in_channels, out_channels))  # 256 -> 64, o/p=64*4
+            layers.append(mpblock_18(self.in_channels, out_channels))  # 256 -> 64, o/p=64*4
 
         return nn.Sequential(*layers)
 
 
 def MPDilGabResNet18(in_channels, num_classes=10):
-    return ResNet(block_18, [2, 2, 2, 2], in_channels, num_classes)
+    return ResNet(mpblock_18, [2, 2, 2, 2], in_channels, num_classes)
